2608705cac31fbc1d825b7a54d5d2b840b1e50f1
zanele <zadubswgc@student.wethinkcode.co.za>
2026-05-13 22:22:10 +0200

    added colors and shapes

--- game.py
+++ game.py
@@ -2,7 +2,18 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode((800,600))
+BLACK = (0,0,0)
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
+PURPLE = (128,0,128)
+WHITE = (255,255,255)
+
+
+WIDTH =800
+HEIGHT = 600
+
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("Let's catch the Colors")
 
 running = True
@@ -11,6 +22,10 @@ while running:
         if i.type == pygame.QUIT:
             running= False
 
-    screen.fill((0,0,0))
-    pygame.display.flip()        
+    screen.fill(BLACK) #
+    
+    pygame.draw.rect(screen,PURPLE,(50,50,100,100))
+    pygame.draw.circle(screen,BLUE,(400,300),50)
+
+    pygame.display.flip()   # This updates the color show at the top     
 pygame.quit()
